970ed074bf9b61b6a0f514c0dc40b5976d2d4f1f
Roy Huaman <RHavila789@gmail.com>
2023-09-09 13:18:05 -0500

    feat: first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Mi primera aplicación</h1>
+      <p>Esta es mi primera aplicación en React, está padrísimo!</p>
+    </div>
+  );
+}
+
+export default App;
